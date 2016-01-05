367fb9aead526baf12ce22e543d12fae00566abd
catasaw <catalina89@msn.com>
2016-01-05 11:09:40 +0100

    Add Statut and Contribute to homepage.

--- magazine/homepageview.py
+++ magazine/homepageview.py
@@ -9,3 +9,4 @@ class HomepageView(View):
     def get(self, request):
         last_issue = Issue.objects.latest('published_at')
         return render(request, 'magazine/homepage.html', {'issue': last_issue})
+        
