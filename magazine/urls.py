3db37b7c859c35386571576a4be688de85b52689
catasaw <catalina89@msn.com>
2016-01-05 15:21:13 +0100

    Add signup view.

--- magazine/urls.py
+++ magazine/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls import url
 
 from . import views
 from magazine.homepageview import HomepageView
+from magazine.signupview import SignupView
+
 
 urlpatterns = [
     url(r'^$', HomepageView.as_view(), name='odisseia'),
+    url(r'^signup/', SignupView.as_view(), name='signup'),
+    #url(r'^createuser/', UserView.as_view(), name='user'),
 ]
