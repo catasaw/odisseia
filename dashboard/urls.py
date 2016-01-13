9a0c2c89a527962695a24e65d97ac6c61a9e6643
catasaw <catalina89@msn.com>
2016-01-13 20:07:14 +0100

    Add new issue view url.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
+    url(r'^newissue/$', views.new_issue_view, name='new_issue_view'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from magazine.models import Issue
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def dashboard(request):
     latest_issues = Issue.objects.order_by('created_at').filter(published_at__isnull=True)[:6]
     return render(request, 'dashboard/issues_dashboard.html', {'issues': latest_issues,})
+
+@login_required
+def new_issue_view(request):
+    return render(request, 'dashboard/new_issue_view.html')
