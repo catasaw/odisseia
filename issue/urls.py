3be7435bf6ece5d64083bf3eeca45d1e0c11a5ed
catasaw <catalina89@msn.com>
2016-02-03 17:55:23 +0100

    Update status to approved or rejected.

--- issue/urls.py
+++ issue/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^issues/pending/$', views.pending_issues_view, name='pending_issues_view'),
+    url(r'^issue/(?P<issue_id>[0-9]+)/(?P<status>(approve|reject)+)/$', views.update_status_issue_view, name='update_status_issue_view'),
 ]
--- issue/views.py
+++ issue/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from magazine.models import Issue
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from datetime import datetime
 
 # Create your views here.
 
@@ -11,3 +12,19 @@ def pending_issues_view(request):
     paginator = Paginator(pending_issues, 12)
     page = request.GET.get('page')
     return render(request, 'issue/pending_issues_view.html', {'issues': pending_issues})
+
+@login_required
+def update_status_issue_view(request, issue_id, status):
+    try: 
+        issue = Issue.objects.get(id = issue_id)
+    except Issue.DoesNotExist:
+        return render(request, 'issue/issue_does_not_exist_view.html')
+    
+    if status == 'approve':
+        issue.status = Issue.APPROVED
+    else:
+        issue.status = Issue.REJECTED
+    
+    issue.status_changed_at = datetime.now()
+    issue.save(update_fields=['status', 'status_changed_at'])
+    return render(request, 'issue/successful_change_status.html')
