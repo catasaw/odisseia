5eddaa0da010c57099f06d5b82e5f1ba042d7f02
catasaw <catalina89@msn.com>
2016-02-29 18:31:40 +0100

    Add robots so that page is not indexed.

--- magazine/urls.py
+++ magazine/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
+
 from . import views
 
 
 urlpatterns = patterns('', 
     url(r'^$', views.homepage, name='odisseia'),
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 );
