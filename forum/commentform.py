5f7e9f073a5203aa1612962ea31f5c7ee4fb3a66
catasaw <catalina89@msn.com>
2016-04-16 14:30:27 +0200

    Transfrom breaklines to html for comment. Display them after comment is submitted.

--- forum/commentform.py
+++ forum/commentform.py
@@ -6,3 +6,8 @@ class CommentForm(forms.Form):
     
     content = forms.CharField(widget=forms.Textarea(attrs={'required': True, 'min_length': 8, 'max_length': 250, 'class' : 'form-control', 'render_value': False, 'placeholder': _('Write your comment here ...'), }),)
     
+    def clean_content(self):
+        # TODO: THere should be a cleaner way to do it!
+        cleaned_content = self.cleaned_data['content']
+        return cleaned_content.replace("\n", "<br />"); 
+    
