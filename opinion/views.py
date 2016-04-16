c8f04f7a6e3332d85983d5f3fe4eaa261e0f0ba6
catasaw <catalina89@msn.com>
2016-04-16 16:51:23 +0200

    Create opinion in another view.

--- opinion/views.py
+++ opinion/views.py
@@ -14,22 +14,10 @@ from datetime import date, datetime
 @login_required
 @csrf_protect
 def opinions_view(request, issue_id):
-    if request.method == 'POST':
-        form = OpinionForm(request.POST)
-        if form.is_valid():
-            opinion = Opinion.objects.create(
-            content=form.cleaned_data['content'],
-            contributor=request.user,
-            issue_id = issue_id,
-            language = form.cleaned_data['language']
-            )
-            return redirect('issue_view', issue_id = issue_id)  
-    else:
-        form = OpinionForm()
     context = {}
     context.update(csrf(request))
-    context['form'] = form
     context['contributor_id'] = request.user.id
+    context['issue_id'] = issue_id
     
     # TODO: Order by most positive votes
     # TODO: Write this in a Manager?
@@ -110,6 +98,23 @@ def is_issue_to_be_pending(issue_id):
     return True
     
     
-    
-    
+def create_opinion_view(request, issue_id):
+    if request.method == 'POST':
+        form = OpinionForm(request.POST)
+        if form.is_valid():
+            opinion = Opinion.objects.create(
+            content=form.cleaned_data['content'],
+            contributor=request.user,
+            issue_id = issue_id,
+            language = form.cleaned_data['language']
+            )
+            return redirect('opinions_view', issue_id = issue_id)  
+    else:
+        form = OpinionForm()
+    context = {}
+    context.update(csrf(request))
+    context['form'] = form
+    context['contributor_id'] = request.user.id
+    context['issue_id'] = issue_id
+    return render(request, 'opinion/create_opinion_view.html', context)
     
