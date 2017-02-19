6689bf89e4bc00bcab430ff84cd414bbf9570838
catasaw <catalina89@msn.com>
2017-02-19 11:32:28 +0100

    Display articles and introductions based on selected language. Fallback is original language.

--- article/views.py
+++ article/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from magazine.models import Article
+from magazine.models import Article, Article_Translation
 from django.http.response import HttpResponse, HttpResponseRedirect
 
 def article_view(request, issue_id, article_id):
@@ -7,11 +7,14 @@ def article_view(request, issue_id, article_id):
     context['issue_id'] = issue_id
     context['article'] = article_id
     
+    article_translation = Article_Translation.objects.filter(article_id = article_id).filter(language_to__iso1_code = request.LANGUAGE_CODE).first()
+    if article_translation:
+        return render(request, 'article/article_view.html', {'article': article_translation})
     
+    # If there is no translation, display original
     try:
-        introduction=Article.objects.get(id=article_id)
+        article= Article.objects.get(id=article_id)
+        return render(request, 'article/article_view.html', {'article': article}) 
     except Article.DoesNotExist:
-        
-        return redirect('/')
-    
-    return render(request, 'article/article_view.html', {'article': introduction})
+        return redirect('/') 
+            
--- introduction/views.py
+++ introduction/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from magazine.models import Introduction
+from magazine.models import Introduction, Introduction_Translation,\
+    Language_Contributor
 from django.http.response import HttpResponse, HttpResponseRedirect
 
 def introduction_view(request, issue_id, introduction_id):
@@ -7,11 +8,14 @@ def introduction_view(request, issue_id, introduction_id):
     context['issue_id'] = issue_id
     context['introduction'] = issue_id
     
+    introduction_translation = Introduction_Translation.objects.filter(introduction_id = introduction_id).filter(language_to__iso1_code = request.LANGUAGE_CODE).first()
+    if introduction_translation:
+        return render(request, 'introduction/introduction_view.html', {'introduction': introduction_translation})
     
+    # If there is no translation, display original
     try:
-        introduction=Introduction.objects.get(id=introduction_id)
+        introduction= Introduction.objects.get(id=introduction_id)
+        return render(request, 'introduction/introduction_view.html', {'introduction': introduction}) 
     except Introduction.DoesNotExist:
-        
-        return redirect('/')
-    
-    return render(request, 'introduction/introduction_view.html', {'introduction': introduction})
+        return redirect('/') 
+            
